0829b299f19855c030c0ec35a2b0166f12c9d14b
t1vr <tanviramd32@gmail.com>
2022-05-07 03:25:07 +0600

    BaseResponse model modified

--- PeerPortal/Application/Wrapper/BaseResponse.cs
+++ PeerPortal/Application/Wrapper/BaseResponse.cs
@@ -9,8 +9,22 @@ namespace Application.Wrapper
     public class BaseResponse<T> where T : class
     {
         public bool Succeeded { get; set; }
-        public string Message { get; set; }
-        public List<string> Errors { get; set; }
-        public T Data { get; set; }
+        public string? Message { get; set; }
+        public List<string>? Errors { get; set; }
+        public T? Data { get; set; }
+
+        BaseResponse() { }
+        BaseResponse(T data,string message=null) 
+        { 
+            Succeeded = true;
+            Message = message;
+            Data = data;
+        }
+        BaseResponse(string message)
+        {
+            Succeeded = false;
+            Message = message;
+        }
+
     }
 }
