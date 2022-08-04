e9e604f161a034741afcc9f3f784e8deaffca6d4
t1vr <tanviramd32@gmail.com>
2022-08-04 20:49:33 +0600

    IBaseService and BaseService implemented

---
+++ PeerPortal/Application/IServices/IBaseService.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.IServices
+{
+    /// <summary>
+    /// Base service interface
+    /// </summary>
+    public interface IBaseService
+    {
+        /// <summary>
+        /// Gets the currently logged in user id
+        /// </summary>
+        /// <returns>Returns the user id</returns>
+        public string GetCurrentUserId();
+    }
+}
---
+++ PeerPortal/Application/Services/BaseService.cs
@@ -0,0 +1,31 @@
+﻿using Application.IServices;
+using Application.Shared.Session;
+
+namespace Application.Services
+{
+    /// <summary>
+    /// Base Service class
+    /// </summary>
+    public class BaseService : IBaseService
+    {
+        private readonly Session _session;
+
+        /// <summary>
+        /// Base Constructor
+        /// </summary>
+        /// <param name="session"></param>
+        public BaseService(Session session)
+        {
+            _session = session;
+        }
+
+        /// <summary>
+        /// Gets the currently logged in user id
+        /// </summary>
+        /// <returns>Returns the user id</returns>
+        public string GetCurrentUserId()
+        {
+            return _session.UserId;
+        }
+    }
+}
