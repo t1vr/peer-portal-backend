b8bdfdf72193da6634787fa7d6710e6cddef9071
t1vr <tanviramd32@gmail.com>
2022-08-09 01:13:01 +0600

    Claim type changed

--- PeerPortal/Application/Services/AccountService.cs
+++ PeerPortal/Application/Services/AccountService.cs
@@ -160,7 +160,7 @@ namespace Application.Services
 
             var claims = new[]
             {
-                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Name, user.UserName),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
                 new Claim("uid", user.Id)
