027ba1572e0cfd77a3a128c7d8c08cc093152584
t1vr <tanviramd32@gmail.com>
2022-09-15 04:05:06 +0600

    Added repository class and interface for TeamUserPermission

---
+++ PeerPortal/Domain/IRepositories/ITeamUserPermissionRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Domain.IRepositories
+{
+    /// <summary>
+    /// Interface to access TeamUserPermission table in database
+    /// </summary>
+    public interface ITeamUserPermissionRepository : IGenericRepository<TeamUserPermission>
+    {
+    }
+}
---
+++ PeerPortal/Infrastructure.Persistence/Repositories/TeamUserPermissionRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Domain.IRepositories;
+using Infrastructure.Persistence.Context;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    /// <summary>
+    /// Repository class for TeamUserPermission entity
+    /// </summary>
+    public class TeamUserPermissionRepository : GenericRepository<TeamUserPermission>, ITeamUserPermissionRepository
+    {
+        public TeamUserPermissionRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
